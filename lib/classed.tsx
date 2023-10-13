1802a8caf012582ac3d1125ec085450f613fe4f9
hungtcs <hungtcs@outlook.com>
2023-10-13 13:43:48 +0800

    feat: update dom element types

--- lib/classed.tsx
+++ lib/classed.tsx
@@ -1,32 +1,23 @@
-import type { ComponentProps, ValidComponent } from "solid-js";
+import type { Component, ComponentProps, ValidComponent } from "solid-js";
 import { Dynamic } from "solid-js/web";
+import domElements, { SupportedHTMLElements } from "./dom-elements";
 
-export function classed<C extends ValidComponent>(component: C) {
+export type ClassedElement<C extends ValidComponent> = (classes: TemplateStringsArray) => Component<ComponentProps<C>>;
+
+function baseClassed<C extends ValidComponent>(component: C): ClassedElement<C> {
   return (classes: TemplateStringsArray) => {
-    return function (props: ComponentProps<C>) {
-      return (
-        <Dynamic<C>
-          component={component}
-          {...props}
-          class={[classes, props.class].join(" ").trim()}
-        />
-      );
+    return function (props) {
+      return <Dynamic<C> component={component} {...props} class={[classes, props.class].join(" ").trim()} />;
     };
   };
 }
 
-classed.a = classed("a");
-classed.p = classed("p");
-classed.h1 = classed("h1");
-classed.h2 = classed("h2");
-classed.h3 = classed("h3");
-classed.h4 = classed("h4");
-classed.h5 = classed("h5");
-classed.h6 = classed("h6");
-classed.div = classed("div");
-classed.span = classed("span");
-classed.main = classed("main");
-classed.aside = classed("aside");
-classed.header = classed("header");
-classed.button = classed("button");
-classed.section = classed("section");
+const classed = baseClassed as typeof baseClassed & {
+  [E in SupportedHTMLElements]: ClassedElement<E>;
+};
+
+for (const element of domElements) {
+  classed[element as SupportedHTMLElements] = classed(element);
+}
+
+export { classed };
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,19 @@
 import { defineConfig } from "vite";
-import solid from "vite-plugin-solid";
 import checker from "vite-plugin-checker";
 import dts from "vite-plugin-dts";
+import solid from "vite-plugin-solid";
 
 export default defineConfig({
   build: {
     lib: {
-      name: "Classed",
       entry: "./lib/index.ts",
-      formats: ["es", "umd"],
+      formats: ["es"],
     },
     rollupOptions: {
       external: ["solid-js", "solid-js/web"],
     },
   },
-  plugins: [
-    dts({ tsconfigPath: "tsconfig.lib.json" }),
-    solid(),
-    checker({ typescript: true }),
-  ],
+  plugins: [dts({ tsconfigPath: "tsconfig.lib.json" }), solid(), checker({ typescript: true })],
   server: {
     port: 1024,
   },
